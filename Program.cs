cf0977b752410e0570d64a00083d6a6003f3d01b
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-03-21 13:36:58 +0200

    add Program.cs , this is the correct Decision

--- Program.cs
+++ Program.cs
@@ -1,22 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
-namespace Test
+namespace Furniture
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string text = " ";
-            string pattern = @"([A-Z][a-z]+): (\d+)";
-            Regex regex = new Regex(pattern);
-            Match match = regex.Match(text);
+            double totalSum = 0;
 
-            Console.WriteLine(match.Groups.Count); // 3
-            Console.WriteLine("Matched text: \"{0}\"", match.Groups[0]);
-            Console.WriteLine("Name: {0}", match.Groups[1]); // Nakov
-            Console.WriteLine("Number: {0}", match.Groups[2]); // 123
+            List<string> finalFurniture = new List<string>();
 
+            while (true)
+            {
+                string line = Console.ReadLine();
+
+                if (line == "Purchase")
+                {
+                    Console.WriteLine("Bought furniture:");
+                   
+                    foreach (var item in finalFurniture)
+                    {
+                        Console.WriteLine(item);
+                    }
+
+                    Console.WriteLine($"Total money spend: {totalSum:F2}");
+
+                    break;
+                }
+
+                string pattern = @"[>]{2}(?<furniture>[A-Z]+[a-z]*)[<]{2}(?<price>\d+\.?\d+)!(?<count>\d+)";
+
+                Regex regex = new Regex(pattern);
+
+                MatchCollection furnitureDetails = regex.Matches(line);
+
+                foreach (Match furniture in furnitureDetails)
+                {
+                    if (furnitureDetails.Count > 0)
+                    {
+                        finalFurniture.Add(furniture.Groups["furniture"].Value);
+                        int quantity = (int.Parse)(furniture.Groups["count"].Value);
+                        totalSum += (((double.Parse)(furniture.Groups["price"].Value)) * quantity);
+                    }
+                }
+            }
         }
     }
 }
